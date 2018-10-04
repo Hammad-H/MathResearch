616db90369fc17e7c8ae380a2be062d77735fe3a
Hammad-H <33607998+Hammad-H@users.noreply.github.com>
2018-10-04 11:50:23 -0400

    Add files via upload

--- BinaryInteger.py
+++ BinaryInteger.py
@@ -30,3 +30,29 @@ class BinaryInteger:
 		for x in difference:
 			count += x
 		return count
+
+	def __add__(self, other):
+		new_bin = [sum(x) for x in zip(self.bin, other.bin)]
+		for x in range(len(new_bin)):
+			if(new_bin[x] % 2 == 0):
+				new_bin[x] = 0
+			else:
+				new_bin[x] = 1
+		str_new = ''.join(str(e) for e in new_bin)
+		return BinaryInteger(str_new)		
+
+	def __mul__(self, other):
+		new_bin = [x*y for x, y in zip(self.bin, other.bin)]
+		str_new = ''.join(str(e) for e in new_bin)
+		return BinaryInteger(str_new)
+
+	def __sub__(self, other):
+		difference = [x-y for x, y in zip(self.bin, other.bin)]
+		for x in range(len(difference)):
+			if (difference[x] == -1):
+				difference[x] = 1
+		count = 0
+		for x in difference:
+			count += x
+		return count
+
